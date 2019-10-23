3ddcdfcde0ca628afe192136e708aafbca1e35b5
Sulemana Awal <awalsulemana3@gmail.com>
2019-10-23 11:33:58 +0100

    Update Results.jsx

--- src/Results.jsx
+++ src/Results.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const Results = (props) => {
 
     return(
-        <div>
-            <h2>{props.data.title }</h2>
-            <p>{props.data.description}</p>
-            <p>{props.data.link}</p>          
+        <div style={{elevation:10,justifyContent:'center',flexDirection:'row'}}>
+            <h2>{props.title }</h2>
+            <p>{props.description}</p>
+            <p>{props.link}</p>          
         </div>
     )
 }
