2940a1c2cdc54439a610b90c58e479330a753a91
Sulemana Awal <awalsulemana3@gmail.com>
2019-10-23 11:35:58 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends React.Component{
     title:"",
     description:"",
     link:"",
+    data:null
   }
 
   handleSubmit = (e) => {
@@ -22,19 +23,7 @@ class App extends React.Component{
       return data.json()
     })
     .then(response => {
-      this.setState({
-        title:response.map(result => {
-          return <Results 
-              title={result[1]}
-              // description={result[2]}
-              // link={result[3]}
-
-          />
-        })
-      })
-      
-
-        
+      this.setState({data:response})
     })
     .catch(error => {
       console.log(`"error" ${error}`)
@@ -43,17 +32,7 @@ class App extends React.Component{
 
 
   }
-      // results.innerHTML = `<li>${response[1]}</li>`;
-      // results.innerHTML = `<li>${response}</li>`;
-      // console.log(results.innerHTML = `<li>${response}</li>`);
-    // })
-    // .catch((error) => {
-    //   console.log("errorrororo" + error)
-    // })
-    // alert(userSearch);
-
-
-
+ 
   render(){
     return (
       <div className="container-fluid">
@@ -66,30 +45,18 @@ class App extends React.Component{
             <button className="btn btn-primary " ><i className="fa fa-search"></i></button>
             
             <div className="container" >
-              {this.state.title}
-              {/* {this.state.description}
-              {this.state.link} */}
+              {this.state.data != null?
+              this.state.data[1].map((data,i)=>{
+                  return <Results title={this.state.data[1][i]} description={this.state.data[2][i]} link={this.state.data[3][i]} />
+              }):null}
               
             </div>
     
           </div>
         </form>
-
-  
-       
-  
       </div>
     );
   }
 } 
 
-  
-  
-
 export default App;
-
-
-
-        // description:response[2].map(result2 => {
-        //   return <Results description={result2} />
-        // })
