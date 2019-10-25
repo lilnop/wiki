233dfd313bcec77b53421a2c004a6c1b44a66ecb
lilnop <lilsnop@gmail.com>
2019-10-25 16:05:30 -0700

    Updates

---
+++ precache-manifest.9f8dfe6164f540a79befafadb6ed7185.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29748501c2252204dcbf9c98217d2b2d",
+    "url": "/wiki-viewer-r/index.html"
+  },
+  {
+    "revision": "2b5bbc78196cb065e89e",
+    "url": "/wiki-viewer-r/static/css/main.1f6d8f9d.chunk.css"
+  },
+  {
+    "revision": "c62bcf12dd613e4785b7",
+    "url": "/wiki-viewer-r/static/js/2.95fd60b3.chunk.js"
+  },
+  {
+    "revision": "2b5bbc78196cb065e89e",
+    "url": "/wiki-viewer-r/static/js/main.34f1a218.chunk.js"
+  },
+  {
+    "revision": "4981f9a10f9c20ae67e2",
+    "url": "/wiki-viewer-r/static/js/runtime-main.ce7ced78.js"
+  },
+  {
+    "revision": "20e42a1aeb4c1d258a62a1e0063a3a2d",
+    "url": "/wiki-viewer-r/static/media/wiki.20e42a1a.png"
+  }
+]);
